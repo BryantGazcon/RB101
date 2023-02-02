f6d2a75f6e93fc8b65a3413f723ea6949441d05f
Bryant Gazcon <bryantgazcon@gmail.com>
2023-02-01 17:45:37 -0800

    easy_set_1

---
+++ Lesson3/easy_1.rb
@@ -0,0 +1,6 @@
+numbers = [1, 2, 2, 3]
+numbers.uniq
+
+puts numbers
+
+#outputs original numbers array, since numbers object was not modified
---
+++ Lesson3/easy_6.rb
@@ -0,0 +1,10 @@
+famous_words = "seven years ago..."
+
+"four score and " + famous_words
+
+"four score and " << famous_words
+
+famous_words.append("four score and")
+
+
+
