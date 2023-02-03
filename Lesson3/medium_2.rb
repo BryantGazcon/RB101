4653e643043de7bfaea6a81a7c0696e4665406ce
Bryant Gazcon <bryantgazcon@gmail.com>
2023-02-02 17:04:31 -0800

    medium_1

---
+++ Lesson3/medium_2.rb
@@ -0,0 +1,5 @@
+var = 40 + 2
+
+puts "the value of 40 + 2 is #{var}"
+
+#strings cannot be concatenated to a string
---
+++ Lesson3/medium_3.rb
@@ -0,0 +1,12 @@
+require 'pry'
+def factors(number)
+  divisor = number
+  factors = []
+  until divisor == 0
+    factors << number / divisor if number % divisor == 0
+    divisor -= 1
+  end
+  factors
+end
+
+p factors(12)
